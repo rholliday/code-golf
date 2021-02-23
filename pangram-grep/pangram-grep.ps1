2c59bd9d49b403e851b2bce95ec26f292f5e2a02
Richard Holliday <rholliday@gmail.com>
2021-02-23 17:32:56 -0500

    Create pangram-grep.ps1

---
+++ pangram-grep/pangram-grep.ps1
@@ -0,0 +1,2 @@
+# Borrowed alphabet array from https://social.technet.microsoft.com/Forums/lync/en-US/0b2a0a4f-df68-486b-8249-735f7c4fad4e/graceful-way-to-create-an-alphabet-array
+[char[]]$z=65..90|%{[char]$_};foreach($a in $args){$c=0;$z|%{if($a-imatch$_){$c++};if($c-eq26){$a}}}
