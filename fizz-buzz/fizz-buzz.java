3a76b443a9c15948aba31dce2ad57db4a4e289f3
Richard Holliday <rholliday@gmail.com>
2021-02-23 11:31:19 -0500

    Update fizz-buzz.java

--- fizz-buzz/fizz-buzz.java
+++ fizz-buzz/fizz-buzz.java
@@ -1,1 +1,1 @@
-class test{public static void main(String[] args){for(int i=1;i<101;i++){String a="";if(i%3==0)a+="Fizz";if(i%5==0)a+="Buzz";if(a=="")a=String.valueOf(i);System.out.println(a);}}}
+class f{public static void main(String[] args){for(int i=1;i<101;i++){String a="";if(i%3==0)a+="Fizz";if(i%5==0)a+="Buzz";if(a=="")a=String.valueOf(i);System.out.println(a);}}}
