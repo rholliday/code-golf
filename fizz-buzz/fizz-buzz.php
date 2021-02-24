74ce5c362a6a5af0576bbf4065eec8f073487daf
Richard Holliday <rholliday@gmail.com>
2021-02-24 15:43:35 -0500

    Update fizz-buzz.php

--- fizz-buzz/fizz-buzz.php
+++ fizz-buzz/fizz-buzz.php
@@ -1,1 +1,2 @@
-for($i=1;$i<101;$i++){$a="";if($i%3==0)$a.="Fizz";if($i%5==0)$a.="Buzz";if($a=="")$a=$i;echo "$a\n";}
+for($i=1;$i<101;$i++){$a="";if($i%3==0)$a="Fizz";if($i%5==0)$a.="Buzz";if(!$a)$a=$i;echo"$a
+";}
