d8c5373a2d117e9413671bd805326edd433c2da5
Richard Holliday <rholliday@gmail.com>
2021-02-24 12:12:24 -0500

    Create tongue-twisters.ps1

---
+++ tongue-twisters/tongue-twisters.ps1
@@ -0,0 +1,20 @@
+$w="wood"
+$c="chuck"
+$u="would";"How much $w $u a $w$c $c,
+If a $w$c could $c $w`?
+A $w$c $u $c all the $w he could $c
+If a $w$c $u $c $w.`n"
+$a="Peter Piper picked"
+$b="peck of pickled peppers"
+"$a a $b.
+A $b $a.
+If $a a $b,
+Where's the $b $a`?`n"
+$s="shells"
+$d="he sells"
+$e="sea$s"
+$f="seashore"
+"S$d $e by the $f,
+The $s s$d are $e, I'm sure.
+So if s$d $e on the $f,
+Then I'm sure s$d $f $s."
