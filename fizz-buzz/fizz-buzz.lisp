d4630e4f26484305ca82ee5145090416352be98a
Richard Holliday <rholliday@gmail.com>
2021-02-25 17:11:49 -0500

    Update fizz-buzz.lisp

--- fizz-buzz/fizz-buzz.lisp
+++ fizz-buzz/fizz-buzz.lisp
@@ -1,1 +1,1 @@
-(loop for i from 1 to 100 do(if(= 0 (mod i 15)) (write-line "FizzBuzz")(if(= 0 (mod i 3)) (write-line "Fizz")(if(= 0 (mod i 5)) (write-line "Buzz")(format t "~a~%" i)))))
+(loop for i from 1 to 100 do(if(= 0(mod i 15))(write-line "FizzBuzz")(if(= 0(mod i 3))(write-line "Fizz")(if(= 0(mod i 5))(write-line "Buzz")(format t "~a~%" i)))))
