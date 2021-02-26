d453f63a0a6610ec94dbdf083dcb60db9a7004c0
Richard Holliday <rholliday@gmail.com>
2021-02-26 16:40:45 -0500

    Update abundant-numbers.ps1

--- abundant-numbers/abundant-numbers.ps1
+++ abundant-numbers/abundant-numbers.ps1
@@ -1,2 +1,2 @@
 # Has a divide by zero error that doesn't trigger in the code.golf interpreter.
-1..200|%{$i=$_;$a=0;1..($i-1)|%{if($i%$_-eq0){$a+=$_}};if($a-gt$i){$i}}
+1..200|%{$i=$_;$a=0;1..($i-1)|%{if(!($i%$_)){$a+=$_}};if($a-gt$i){$i}}
