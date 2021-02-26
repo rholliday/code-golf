d7819b359b5add2b4fe9ce04e0db461b626510e5
Richard Holliday <rholliday@gmail.com>
2021-02-26 15:06:20 -0500

    Update niven-numbers.ps1

--- niven-numbers/niven-numbers.ps1
+++ niven-numbers/niven-numbers.ps1
@@ -1,3 +1,3 @@
 # toCharArray method was ridiculous.
 # Also only -split works, not .split().
-0..100|%{if($_%($_ -split''|measure -sum).sum-eq0){$_}}
+0..100|%{if(!($_%($_ -split''|measure -sum).sum)){$_}}
