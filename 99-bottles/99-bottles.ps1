1c15b8ee467351e9c89629eabb4fd9b060962f91
Richard Holliday <rholliday@gmail.com>
2021-02-26 12:10:46 -0500

    Update 99-bottles.ps1

--- 99-bottles/99-bottles.ps1
+++ 99-bottles/99-bottles.ps1
@@ -1,1 +1,3 @@
-99..0|%{if($_-gt1){$a="$_ bottles of beer";$b="Take one down and pass it around";$t=$_-1;if($t-eq1){$c="1 bottle"}else{$c="$t bottles"}};if($_-eq1){$a="1 bottle of beer";$b="Take one down and pass it around";$c="no more bottles"};if($_-eq0){$a="No more bottles of beer";$b="Go to the store and buy some more";$c="99 bottles"};"$a on the wall, $($a.toLower()).`n$b, $c of beer on the wall.`n"}
+$x=" bottles";$y=" beer";$z="Take one down and pass it around";$w=" on the wall";99..0|%{if($_-gt1){$a="$_$x of$y";$b=$z;$t=$_-1;if($t-eq1){$c="1 bottle"}else{$c="$t$x"}};if($_-eq1){$a="1 bottle of$y";$b=$z;$c="no more$x"};if($_-eq0){$a="No more$x of$y";$b="Go to the store and buy some more";$c="99$x"};"$a$w, $($a.toLower()).
+$b, $c of$y$w.
+"}
