4e1a0e2266318bacf405c385b5795b2ed25e7bbc
Richard Holliday <rholliday@gmail.com>
2021-02-26 16:29:13 -0500

    Update pernicious-numbers.ps1

--- pernicious-numbers/pernicious-numbers.ps1
+++ pernicious-numbers/pernicious-numbers.ps1
@@ -1,1 +1,1 @@
-0..50|%{$a=([Convert]::ToString($_,2) -split''|measure -sum).sum;$b=0;1..$a|%{if($a%$_-eq0){$b++}};if($b-eq2){$_}}
+0..50|%{$a=([Convert]::ToString($_,2)-split''|measure -sum).sum;$b=0;1..$a|%{if(!($a%$_)){$b++}};if($b-eq2){$_}}
