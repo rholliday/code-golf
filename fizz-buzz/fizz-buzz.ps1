5e550d38ccfdc145b210d8e897764065876ef9a3
Richard Holliday <rholliday@gmail.com>
2021-02-26 11:43:46 -0500

    Update fizz-buzz.ps1

--- fizz-buzz/fizz-buzz.ps1
+++ fizz-buzz/fizz-buzz.ps1
@@ -1,2 +1,2 @@
 # Should throw an error on removing a when it doesn't exist but doesn't on the first run.
-1..100|%{rv a;if($_%3-eq0){$a="Fizz"}if($_%5-eq0){$a+="Buzz"}if(!$a){$a=$_}$a}
+1..100|%{rv a;if(!($_%3)){$a="Fizz"}if(!($_%5)){$a+="Buzz"}if(!$a){$a=$_}$a}
