8d19335663096f26cb9647092fbd80336f8dcdc5
Richard Holliday <rholliday@gmail.com>
2021-02-26 12:16:32 -0500

    Update tongue-twisters.ps1

--- tongue-twisters/tongue-twisters.ps1
+++ tongue-twisters/tongue-twisters.ps1
@@ -1,23 +1,23 @@
 $w=" wood"
 $c="chuck"
 $u=" would"
-"How much$w$u a$w$c $c,
-If a$w$c could $c$w`?
-A$w$c$u $c all the$w he could $c
-If a$w$c$u $c$w.
-"
 $a="Peter Piper picked"
 $b=" peck of pickled peppers"
-"$a a$b.
-A$b $a.
-If $a a$b,
-Where's the$b $a`?
-"
 $s="shells"
 $d="he sells"
 $e=" sea$s"
 $f=" seashore"
-"S$d$e by the$f,
+"How much$w$u a$w$c $c,
+If a$w$c could $c$w`?
+A$w$c$u $c all the$w he could $c
+If a$w$c$u $c$w.
+
+$a a$b.
+A$b $a.
+If $a a$b,
+Where's the$b $a`?
+
+S$d$e by the$f,
 The $s s$d are$e, I'm sure.
 So if s$d$e on the$f,
 Then I'm sure s$d$f $s."
