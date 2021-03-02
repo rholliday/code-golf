bc053e30cbb0f4a792549d7ff9a0dc7ca845e9c3
Richard Holliday <rholliday@gmail.com>
2021-03-02 14:55:48 -0500

    Update emirp-numbers.ps1

--- emirp-numbers/emirp-numbers.ps1
+++ emirp-numbers/emirp-numbers.ps1
@@ -1,5 +1,5 @@
-# NOT WORKING, hits 7s timeout.
-# Also inelegant/cheating to skip 1-11 instead of properly handling them?
-# Tried [array]::Reverse() method also.
-function p($i){$a=0;1..$i|%{if(!($i%$_)){$a++}};if($a-eq2){return $true}}
-12..1000|%{if(p $_){$c=$_ -split'';if(p($c[($c.count-1)..0] -join'')){$_}}}
+# Found square root method to speed up processing.
+# Still slightly cheating to save a character stopping at 991?
+# [array]::Reverse() is slightly longer bytewise.
+function p($i){$a=0;2..[math]::Sqrt($i)|%{if(!($i%$_)){$a++}};if(!$a){1}}
+1..991|%{if(p $_){$c=$_-split'';$d=$c[($c.count-1)..0]-join'';if($_-ne$d){if(p($d)){$_}}}
