4d584b49d13d7eb733b14d52617d7f77ed4ac742
Richard Holliday <rholliday@gmail.com>
2021-03-02 16:30:09 -0500

    Update kolakoski-sequence.rb

--- kolakoski-sequence/kolakoski-sequence.rb
+++ kolakoski-sequence/kolakoski-sequence.rb
@@ -1,19 +1,13 @@
 # Since I'm shameless, adapted code from https://rosettacode.org/wiki/Kolakoski_sequence#Ruby
 # (But an admin clearly just pasted in the actual sequence for every language on this one so meh.)
-def create_generator(ar)
-  Enumerator.new do |y|
-    cycle = ar.cycle
-    s = []
-    loop do
-      t = cycle.next
-      s.push(t)
-      v = s.shift
-      y << v
-      (v-1).times{s.push(t)}
-    end
-  end
+def r(g)Enumerator.new do|y|c=g.cycle
+s=[]
+loop do
+t=c.next
+s.push(t)
+v=s.shift
+y<<v
+(v-1).times{s.push(t)}end
 end
- 
-[[1000, [1,2]]].each do |num,ar|
-  puts(create_generator(ar).take(num).join(" "))
 end
+[[1000,[1,2]]].each do|n,g|puts(r(g).take(n).join(" "))end
