9f6e7d3049244e622ac31bd3e1f0dcc21c3d3061
Richard Holliday <rholliday@gmail.com>
2021-03-03 17:21:05 -0500

    Update diamonds.ps1

--- diamonds/diamonds.ps1
+++ diamonds/diamonds.ps1
@@ -1,6 +1,4 @@
-# NOT WORKING.
-# This is a MESS.
-# Everything works except the second diamond.
+# What an ugly piece of code to try and shorten. Finally working at least.
 for($i=1;$i-le9;$i++)
 {
     " "*9+"1"
@@ -23,20 +21,23 @@ for($i=1;$i-le9;$i++)
             $m--
         }
         $m+=2
-        ($i-1)..2|%{
-            $a="1"
-            2..$_|%{
-                $a+=$($_)
-            }
-            if($_-gt2)
-            {
-                ($_-1)..2|%{
+        if($i-gt2)
+        {
+            ($i-1)..2|%{
+                $a="1"
+                2..$_|%{
                     $a+=$($_)
                 }
+                if($_-gt2)
+                {
+                    ($_-1)..2|%{
+                      $a+=$($_)
+                    }
+                }
+                $a+="1"
+                " "*$m+$a
+                $m++
             }
-            $a+="1"
-            " "*$m+$a
-            $m++
         }
         " "*9+"1"
     }
